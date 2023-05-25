ee8330cc3b6e14a598307cdc3e36c09ef004b653
marchukmx <mmmarch1k@gmail.com>
2023-05-25 20:29:25 +0300

    #4

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-#2
+#4
 import requests
-
 from bs4 import BeautifulSoup
-response = requests.get('https://ru.wikipedia.org/')
-
+response = requests.get('https://www.example.com/')
 if response.status_code == 200:
-    soup = BeautifulSoup(response.content,"html.parser")
-    img = soup.find_all("img")
-    for i in img:
-        print('https://' + i["src"])
+    soup = BeautifulSoup(response.text , features="html.parser")
+    for script in soup.find_all(["style","script"]):
+        script.extract() #вирізає приколи HTML
+    text = ' '.join(soup.stripped_strings)
+    words = len(text.split())
+    print(words)
